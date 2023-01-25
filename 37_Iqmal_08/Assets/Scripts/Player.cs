2a62114f58d9cfd4b22c4c2a658903237a8688e0
Md.Iqmal <111185577+User101fafdasfsfsa@users.noreply.github.com>
2023-01-26 00:52:47 +0800

    Lab08

---
+++ 37_Iqmal_08/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, - 4f, 4f), transform.position.z);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
---
+++ 37_Iqmal_08/Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text ScoreText;
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = "Score :" + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            score += 10;
+            Destroy(collision.gameObject);
+            ScoreText.text = "Score :" + score;
+        }
+    }
+}
